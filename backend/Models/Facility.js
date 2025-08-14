166f0d8cdfcdddb48fe0f0cc05e173bfe1575d04
Esther Wavinya Ndavi <estherwavinya80@gmail.com>
2025-08-14 17:51:32 +0300

    Facility model

--- backend/Models/Facility.js
+++ backend/Models/Facility.js
@@ -1,5 +1,15 @@
+// Mongoose is an ODM(Object Data Modelling) library for MongoDB. It lets you define schemas and interact with the database using JavaScript obejcts instead of raw queries
 import mongoose from "mongoose";
 
+// This schema describes what fields a facility will have:
+// name, String, required
+// location, String, required
+// description, String, optional
+// pricePerHour, Number, required, cost per hour to book this facility
+// isAvailable, Boolean, default true, indicates if the facility can be booked
+// The { timestamps: true } option automatically adds: 
+// createdAt, when the document was created
+// updatedAt, when the document was last modified
 const facilitySchema = new mongoose.Schema(
   {
     name: { type: String, required: true },
@@ -11,4 +21,31 @@ const facilitySchema = new mongoose.Schema(
   { timestamps: true }
 );
 
+// This compiles the schema into a model named "Facility". When used, it will interact with the facilities collection in MongoDB. 
 export default mongoose.model("Facility", facilitySchema);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
